043ec5e923ad1f4e3cd4685af8f80bc2255836b0
jwang307 <john.wang1726@gmail.com>
2020-10-03 22:23:50 -0700

    working on auto annotator

--- AutoAnnotate/src/main/java/Main.java
+++ AutoAnnotate/src/main/java/Main.java
@@ -1,20 +1,47 @@
 import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.biojava.nbio.core.sequence.ProteinSequence;
+import org.biojava.nbio.core.sequence.io.FastaReaderHelper;
+import org.biojava.nbio.core.sequence.io.FastaSequenceParser;
+import org.biojava.nbio.genome.parsers.gff.Feature;
+import org.biojava.nbio.genome.parsers.gff.FeatureList;
+import org.biojava.nbio.genome.parsers.gff.GFF3Reader;
+
 
 public class Main {
+
     public static void main(String[] args) throws  IOException {
-        BufferedReader br = new BufferedReader(new FileReader("in.in"));
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("out.out")));
-        String line;
-        for (int i = 0; i < 14; i++) {
-            if (i<9) {
-                System.out.println(br.readLine());
-            } else {
-                br.readLine();
-            }
+        if (args.length < 3) throw new IllegalArgumentException("Specify 3 parameters: gff input file name, fasta input file name, and gff output file name");
+        FeatureList gffInFeatures = GFF3Reader.read(args[0]);
+        String[] cmd = {"hmmsearch", "repprofile.hmm", args[1], "> test.out"};
+        Process hmmRun = Runtime.getRuntime().exec(cmd);
+
+        try{
+            Thread.sleep(5000);
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
-        while (!(line= br.readLine()).equals("")) {
-            pw.println(line);
-        }
-        pw.close();
+
+        List<String> hits = readHMMER();
+
+
+        //remove all the features that aren't ORFs and removed duplicate ORFs
+        //run exec command hmmsearch on fasta file
+        //read in results and remove all gff annotations that don't fit
+        //return gff file
+        //TODO: INSTALL JAVA ON UBUNTU IN ORDER TO TEST
     }
+
+    public static void cleanGFF(FeatureList gffFeatures) {
+
+    }
+
+    public static List<String> readHMMER() {
+      return null;
+    }
+
 }
